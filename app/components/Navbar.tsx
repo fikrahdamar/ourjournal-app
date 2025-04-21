4e5c10b31b3c3b7644c76abe379dad4d08f178ba
fikrahdamar <fikrahdamar7@gmail.com>
2025-04-21 23:05:47 +0700

    Done setup auth

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import { auth } from "@/auth";
-import { signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import AuthButton from "./AuthButton";
+import { Session } from "next-auth";
 
 const Navbar = async () => {
-  const session = await auth();
+  const session: Session | null = await auth();
 
   return (
     <header className="bg-bggrey px-5 py-3 shadow-sm">
@@ -13,33 +13,7 @@ const Navbar = async () => {
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={140} height={50} />
         </Link>
-        <div className="flex items-center text-black">
-          {session?.user ? (
-            <>
-              <Link href="/startup/create">
-                <span>Create</span>
-              </Link>
-
-              <button onClick={signOut}>
-                <span>Logout</span>
-              </button>
-
-              <Link href={`/user/${session?.id}`}>
-                <span>{session?.user?.name}</span>
-              </Link>
-            </>
-          ) : (
-            <form
-              action={async () => {
-                "use server";
-                await signIn("github");
-              }}
-            >
-              <button type="submit">Login</button>
-            </form>
-          )}
-          ;
-        </div>
+        <AuthButton session={session} />
       </nav>
     </header>
   );
